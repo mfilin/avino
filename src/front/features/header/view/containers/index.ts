661bd79f9c7d59f9cd8f2921ac8bde366b426080
Максим Филин <esoul7@yandex.ru>
2025-02-17 22:27:39 +0300

    add top-nav

--- src/front/features/header/view/containers/index.ts
+++ src/front/features/header/view/containers/index.ts
@@ -1,2 +1,4 @@
 export { default as TopMenuContainer } from './TopMenuContainer';
+
+export { default as TopNavContainer } from './TopNavContainer';
 export { default as HeaderContainer } from './HeaderContainer';
