c763e06473debe893fc04cb9c8470f945b10a83c
Максим Филин <esoul7@yandex.ru>
2025-04-29 19:28:31 +0300

    test console.log

--- src/front/features/header/view/components/CategoryMenu/index.tsx
+++ src/front/features/header/view/components/CategoryMenu/index.tsx
@@ -43,7 +43,7 @@ const CategoryMenu: React.FC<IOwnProps> = (props) => {
       <ul className="nav-menu">
         {catalogSlugOrdered.map((code) => {
           const item = cache?.catalog[code];
-          // console.log(item);
+          console.log(cache['cognac-all']);
           return (
             <li
               key={code}
