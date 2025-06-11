b90c81f7a8c023dc0390b8d6bfca82368a9d17c0
Максим Филин <esoul7@yandex.ru>
2025-06-11 23:21:31 +0300

    fix last

--- src/front/features/header/view/components/ArrowIcon/index.tsx
+++ src/front/features/header/view/components/ArrowIcon/index.tsx
@@ -13,7 +13,7 @@ const ArrowIcon: React.FC<ArrowIconProps> = ({ className }) => {
   return (
     <i className={className}>
       <svg className="ico-svg" viewBox="0 0 7 12" xmlns="http://www.w3.org/2000/svg">
-        <use xlinkHref="img/sprites/sprite.svg#nav_arrow" xmlnsXlink="http://www.w3.org/1999/xlink"></use>
+        <use xlinkHref="/images/sprites/sprite.svg#nav_arrow" xmlnsXlink="http://www.w3.org/1999/xlink"></use>
       </svg>
     </i>
   );
