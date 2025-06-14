bb6ecb0997c4eee0f9ff4e3e44ebc1eb20853cd6
Максим Филин <esoul7@yandex.ru>
2025-06-14 11:21:06 +0300

    fix swiper популярных брендов

--- src/front/pages/IndexPage.tsx
+++ src/front/pages/IndexPage.tsx
@@ -332,30 +332,34 @@ const IndexPage: NextPage = () => {
           
           {/* Вас могут заинтересовать */}
 
-          <div className="section">
-              <div className="container">
-                  <div className="section__header">
-                      <h2 className="section__title">Популярные бренды</h2>
-                  </div>
+        <div className="section">
+            <div className="container">
+                <div className="section__header">
+                    <h2 className="section__title">Популярные бренды</h2>
+                </div>
 
-                  <div className="swiper" data-brands>
-                      <div className="swiper-wrapper">
-                          {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
-                              <div className="swiper-slide" key={num}>
-                                  <div className="brand">
-                                      <div className="brand__logo">
-                                          <img src={`images/content/brands/brand_logo__${num.toString().padStart(2, '0')}.png`}
-                                            className="img-contain" alt="" />
-                                      </div>
-                                  </div>
-                              </div>
-                          ))}
-                      </div>
-                  </div>                  
-              </div>
-          </div> {/* Популярные бренды */}
+                <Swiper
+                    modules={[Navigation]}
+                    spaceBetween={50}
+                    slidesPerView={"auto"}
+                    className="swiper"
+                    data-brands
+                >
+                    {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
+                        <SwiperSlide key={num}>
+                            <div className="brand">
+                                <div className="brand__logo">
+                                    <img src={`images/content/brands/brand_logo__${num.toString().padStart(2, '0')}.png`}
+                                      className="img-contain" alt="" />
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </div> {/* Популярные бренды */}
 
-          <div className="section">
+        <div className="section">
             <div className="container">
                 <div className="section-wrapper">
                     <HowBuyBanner />
