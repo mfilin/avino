5370ac4c161dc8b63eb64b83fcc83bdb8a8e4f61
Максим Филин <esoul7@yandex.ru>
2025-08-31 20:57:34 +0300

    add tabs for comments and reviews with js

--- src/front/features/singleProduct/view/components/SingleProductCard/index.tsx
+++ src/front/features/singleProduct/view/components/SingleProductCard/index.tsx
@@ -98,6 +98,34 @@ const SingleProductCard: React.FC<IOwnProps> = ({
     tags.push('discount');
   }
 
+  const handleTabClick = React.useCallback((event) => {
+    const tabsArray = document.querySelectorAll('[data-tabs]');
+    if (tabsArray.length > 0) {
+      if (event.target.closest('[data-tabs-nav]')) {
+        const tabs = event.target.closest('[data-tabs]');
+        const tabsNavs = tabs.querySelectorAll('[data-tabs-nav]');
+        const tabsContent = tabs.querySelectorAll('[data-tabs-target]');
+        const tabsPath = event.target.closest('[data-tabs-nav]').dataset.tabsNav;
+
+        tabsNavs.forEach(elem => {
+          elem.classList.remove('active');
+        });
+        tabsContent.forEach(elem => {
+          elem.classList.remove('active');
+        });
+        tabs.querySelector(`[data-tabs-nav="${tabsPath}"]`).classList.add('active');
+        tabs.querySelector(`[data-tabs-target="${tabsPath}"]`).classList.add('active');
+      }
+    }
+  }, []);
+
+  React.useEffect(() => {
+    document.addEventListener('click', handleTabClick);
+    return () => {
+      document.removeEventListener('click', handleTabClick);
+    };
+  }, [handleTabClick]);
+
   return (
     <>
     <div className="section">
